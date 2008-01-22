52b197bd3fc76b439fe924d1ca4eefea9ce90626
Tony Mack <tmack@cs.princeton.edu>
2008-01-22 17:09:10 +0000

    fix errors

--- qaapi/qa/modules/plc/stop.py
+++ qaapi/qa/modules/plc/stop.py
@@ -9,15 +9,15 @@ class stop(Test):
     """
 
     def call(self, system_type, root_dir):
-
- 	stop command = " /sbin/service plc stop "
+        
+ 	stop_command = " /sbin/service plc stop "
 	full_command = ""
 	if system_type in ['vserv', 'vserver']:
 	    full_command += " vserver %(root_dir)s exec "
 	elif system_type in ['chroot']:
 	    pass
 	else:
-	    raise Exception, "Invalid system type %(system_type)s" locals() 	
+	    raise Exception, "Invalid system type %(system_type)s" % locals() 	
 	
 	full_command += stop_command % locals()
 
