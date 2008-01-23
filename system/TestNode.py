19321f9c4319c7d4f35ebdf9256c85e648b55abd
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2008-01-23 15:24:22 +0000

    *Now the TestFrame is Qemu emulation capable *creating bootcd for the Qemu node, *Run the node on a distant host machine

--- system/TestNode.py
+++ system/TestNode.py
@@ -61,27 +61,58 @@ class TestNode:
         self.test_plc.server.DeleteNode(auth,self.name())
 
     def conffile(self,image,hostname,path):
-        template='%s/template-vmplayer/node.vmx'%(path)
-        actual='%s/vmplayer-%s/node.vmx'%(path,hostname)
-        sed_command="sed -e s,@BOOTCD@,%s,g %s > %s"%(image,template,actual)
-        utils.header('Creating %s from %s'%(actual,template))
-        utils.system(sed_command)
-
+        model=self.node_spec['node_fields']['model']
+        host_machine=self.node_spec['node_fields']['host_machine']    
+        if model.find("vmware") >= 0:
+            template='%s/template-vmplayer/node.vmx'%(path)
+            actual='%s/vmplayer-%s/node.vmx'%(path,hostname)
+            sed_command="sed -e s,@BOOTCD@,%s,g %s > %s"%(image,template,actual)
+            utils.header('Creating %s from %s'%(actual,template))
+            utils.system(sed_command)
+        elif  model.find("qemu") >= 0:
+            mac=self.node_spec['network_fields']['mac']
+            dest_dir="qemu-%s"%(hostname)
+            utils.header('Storing the mac address for node %s'%hostname)
+            file=open(path+'/qemu-'+hostname+'/MAC','a')
+            file.write('%s\n'%mac)
+            file.write(dest_dir)
+            file.close()
+            utils.header ('Transfert of configuration files for node %s into %s '%(hostname,host_machine))
+            cleandir_command="ssh root@%s rm -rf %s"%(host_machine, dest_dir)
+            createdir_command = "ssh root@%s mkdir -p  %s"%(host_machine, dest_dir)
+            utils.system(cleandir_command)
+            utils.system(createdir_command)
+            scp_command = "scp -r %s/qemu-%s/* root@%s:/root/%s"%(path,hostname,host_machine,dest_dir)
+            utils.system(scp_command)
+        
     def create_boot_cd(self,path):
+        model=self.node_spec['node_fields']['model']
         node_spec=self.node_spec
         hostname=node_spec['node_fields']['hostname']
-        utils.header('Initializing vmplayer area for node %s'%hostname)
-        clean_dir="rm -rf %s/vmplayer-%s"%(path,hostname)
-        mkdir_command="mkdir -p %s/vmplayer-%s"%(path,hostname)
-        tar_command="tar -C %s/template-vmplayer -cf - . | tar -C %s/vmplayer-%s -xf -"%(path,path,hostname)
-        utils.system(clean_dir)
-        utils.system(mkdir_command)
-        utils.system(tar_command);
-        utils.header('Creating boot medium for node %s'%hostname)
         encoded=self.test_plc.server.GetBootMedium(self.test_plc.auth_root(), hostname, 'node-iso', '')
         if (encoded == ''):
             raise Exception, 'boot.iso not found'
-        file=open(path+'/vmplayer-'+hostname+'/boot_file.iso','w')
+            
+        if model.find("vmware") >= 0:
+            utils.header('Initializing vmplayer area for node %s'%hostname)
+            clean_dir="rm -rf %s/vmplayer-%s"%(path,hostname)
+            mkdir_command="mkdir -p %s/vmplayer-%s"%(path,hostname)
+            tar_command="tar -C %s/template-vmplayer -cf - . | tar -C %s/vmplayer-%s -xf -"%(path,path,hostname)
+            utils.system(clean_dir)
+            utils.system(mkdir_command)
+            utils.system(tar_command);
+            utils.header('Creating boot medium for node %s'%hostname)
+            file=open(path+'/vmplayer-'+hostname+'/boot_file.iso','w')
+        elif  model.find("qemu") >= 0:
+            clean_dir="rm -rf %s/qemu-%s"%(path,hostname)
+            mkdir_command="mkdir -p %s/qemu-%s"%(path,hostname)
+            utils.system(clean_dir)
+            utils.system(mkdir_command)
+            copy_command="cp -r  %s/template-Qemu/* %s/qemu-%s"%(path,path,hostname)
+            utils.system(copy_command)
+            utils.header('Creating boot medium for node %s'%hostname)
+            file=open(path+'/qemu-'+hostname+'/boot_file.iso','w')
+
         file.write(base64.b64decode(encoded))
         file.close()
         utils.header('boot cd created for %s'%hostname)
@@ -104,4 +135,12 @@ class TestNode:
         utils.system('cd %s/vmplayer-%s ; DISPLAY=%s vmplayer node.vmx < /dev/null >/dev/null 2>/dev/null &'%(path,hostname,display))
         
     def start_qemu (self, options):
-        utils.header ("TestNode.start_qemu: not implemented yet")
+        host_machine=self.node_spec['node_fields']['host_machine']
+        hostname=self.node_spec['node_fields']['hostname']
+        path=options.path
+        display=options.display
+        dest_dir="qemu-%s"%(hostname)
+        utils.header('Starting qemu for node %s '%(hostname))
+        utils.system("ssh root@%s ~/%s/env-qemu start "%(host_machine, dest_dir ))
+        utils.system("ssh  root@%s DISPLAY=%s  ~/%s/start-qemu-node %s & "%( host_machine, display, dest_dir, dest_dir))
+        
