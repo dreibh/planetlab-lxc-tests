e5079ff953731d7e24c9c15d5ab38642b09c5c19
Tony Mack <tmack@cs.princeton.edu>
2008-01-24 18:22:05 +0000

    updated to reflect recent changes in qash/QAAPI

--- qaapi/selftest.py
+++ qaapi/selftest.py
@@ -14,26 +14,26 @@ url = None
 node = config.TEST_NODE_HOSTNAME_1
 
 
-#plc.install(sys_type, vserver_name, url)
-#plc.config(sys_type, vserver_name)
-#plc.start(sys_type, vserver_name)
+#plc_install(sys_type, vserver_name, url)
+#plc_config(sys_type, vserver_name)
+#plc_start(sys_type, vserver_name)
 
 # Add test site, node, person and slice data
 # Adds slice to node and person to slice 
-#plc.add_test_data()
+#add_test_data()
 
 # Boot test node and confirm boot state
-#plc.boot_node(node)
-#if plc.get_boot_state(node) not in ['boot']:
+#boot_node(node)
+#if get_boot_state(node) not in ['boot']:
 #    raise Exception, "%(node)s not fully booted" % locals()
 
 # Restart node manager on the node
 #priv_key_path = "%(vserver_path)/etc/planetlab/root_ssh_key.rsa" % locals() 
 #restart_nm = 'service nm restart'	
-#plc.remote_call(priv_key_path, node, restart_nm)
+#remote_call(priv_key_path, node, restart_nm)
 
 # Try to access the test  slice on the test node
 #email = config.TEST_PERSON_EMAIL
 #slice = config.TEST_SLICE_NAME
-#plc.access_slice(email, slice, node)
+#access_slice(email, slice, node)
    
