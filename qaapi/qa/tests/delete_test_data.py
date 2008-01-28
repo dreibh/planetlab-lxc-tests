08de3f7cc776830131a70ec329a7fd2f575bfc0e
Tony Mack <tmack@cs.princeton.edu>
2008-01-28 17:19:23 +0000

    rewritten as a plcsh script

--- qaapi/qa/tests/delete_test_data.py
+++ qaapi/qa/tests/delete_test_data.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env /usr/share/plc_api/plcsh
+
 import os, sys
 from Test import Test
 from qa import utils
@@ -10,13 +12,10 @@ class delete_test_data(Test):
 
     def call(self):
 	
-	api = self.config.api
-        auth = self.config.auth
-	
 	site_login_base = self.config.TEST_SITE_LOGIN_BASE
 	
 	# Deleting the site should delete everything associated with it
-	api.DeleteSite(auth, site_login_base)
+	DeleteSite(site_login_base)
 	if self.config.verbose:
 	    utils.header("Test data deleted")
 
@@ -24,4 +23,4 @@ class delete_test_data(Test):
 	
 if __name__ == '__main__':
     args = tuple(sys.argv[1:])
-    delete_test_data(*args)		
+    delete_test_data()(*args)		
