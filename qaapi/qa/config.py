6c99573a47425d302aa6745cf7d164670db5637b
Tony Mack <tmack@cs.princeton.edu>
2008-03-18 21:02:50 +0000

    get loadable config data from object in a pythong config file. simplifies configuring multiple objects/plcs

---
+++ qaapi/qa/config.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+sites = []
+persons = []
+slices = []
+
+plcs = [
+	{'name': 'TestPLC',
+         'host': 'localhost',
+         'ip': '127.0.0.1',
+         'rootkey': '/home/tmack/.ssh/plc-root',
+         'url': 'https://localhost.localdomain/',
+         'port': '443'
+        }
+	]
+
+sites = [
+	{'plc': 'TestPLC',
+	 'name': 'TestSite1',
+	 'login_base': 'ts',
+	 'enabled': True,
+	 'abbreviated_name': 'Test1',
+	 'max_slices': 100,
+	 'is_public': True,
+	 'url': 'http://pl-test.planet-lab.org'
+	}
+	]
+
+nodes = [
+	{'plc': 'TestPLC',
+	 'site': 'ts',
+	 'hostname': 'vm1.paris.cs.princeton.edu',
+	 'host': 'localhost',
+	 'type': 'virtual',
+	 'nodenetworks' : [{'type': 'ipv4',
+			   'method': 'static',
+			   'ip': '10.0.2.16',
+			   'gateway': '10.0.2.2',
+			   'dns1': '10.0.2.2',
+			   'network': '10.0.2.0',
+			   'netmask': '255.255.255.0',
+			   'broadcast': '10.0.2.255'
+			   }]
+	}
+	]
+
+slices = [
+	{'plc': 'TestPLC', 
+	 'name': 'ts_slice1',
+	 'instantiation': 'plc-instantiated',
+	 'max_nodes': 1000,
+	 'description': 'blank',
+	 'url': 'http://test.org',
+	 'nodes': ['vm1.paris.cs.princeton.edu']
+	}
+	]
+
+persons = [
+	{'plc': 'TestPLC',
+	 'first_name': 'fname',
+	 'last_name': 'lname',
+	 'password': 'password',
+	 'email': 'person@cs.princeton.edu',
+	 'roles': ['user', 'pi'],
+	 'sites': ['ts'], 
+	 'slices': ['ts_slice1']
+	}
+	]
