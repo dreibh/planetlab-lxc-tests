44d699a3eb760d631169eef4418bd2ef84ae63da
Tony Mack <tmack@cs.princeton.edu>
2008-07-17 21:31:05 +0000

    minor updates

--- qaapi/qa/Slices.py
+++ qaapi/qa/Slices.py
@@ -4,7 +4,7 @@ from Table import Table
 
 class Slice(dict, Remote):
 
-     fields = {
+    fields = {
 	'plcs': ['TestPLC'],		     # which plc does this slice belong to	
 	'name': None,
 	'instantiation': 'plc-instantiated',
@@ -13,17 +13,21 @@ class Slice(dict, Remote):
 	'url': None,
 	'key': None,			     # any valid ssh key
 	'nodes': [],			     # nodes where this slice runs
+	'persons': [], 			     # persons on this slice
 	'tests_path': '/usr/share/tests/',   	
-	'tests': []                          # which test to run. None or empty list means run all
+	'tests': [],                          # which test to run. None or empty list means run all
 	}
 
-     def __init__(self, fields = {}):
+    def __init__(self, config, fields = {}):
 
 	dict.__init__(self, self.fields)
-	
+	self.config = config
 	self.update(fields)
 
+	
+	    	
 class Slices(Table):
-    def __init__(self, slices):
-	slicelist = [Slice(slice) for slice in slices]
-	Table.__init__(self, slicelist)				 	  			 	 	
+    def __init__(self, config, slices):
+	slicelist = [Slice(config, slice) for slice in slices]
+	Table.__init__(self, slicelist)
+	self.config = config				 	  			 	 	
