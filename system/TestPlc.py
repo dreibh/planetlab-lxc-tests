f5a3fc8dba7e584c3945f2009722d2a98937f25a
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2008-09-25 20:00:51 +0000

    oops

--- system/TestPlc.py
+++ system/TestPlc.py
@@ -395,7 +395,7 @@ class TestPlc:
     ### install_rpm 
     def install_rpm(self):
         return self.run_in_guest("yum -y install myplc-native")==0 \
-            and self.run_in_guest("yum -y install noderepo-%s-%s"%(self.options.pldistro,self.options.arch))
+            and self.run_in_guest("yum -y install noderepo-%s-%s"%(self.options.pldistro,self.options.arch))==0
 
     ### 
     def configure(self):
