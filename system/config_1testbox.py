79c0d198628d71834a84c591640c15fa268b6501
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2008-11-20 17:06:45 +0000

    trying out another mapping

--- system/config_1testbox.py
+++ system/config_1testbox.py
@@ -7,26 +7,26 @@ from TestMapper import TestMapper
 def config (plcs, options):
 
     if options.arch == "i386":
-        testbox1 = 'testbox32.onelab.eu'
-        testbox2 = 'testbox32.onelab.eu'
-        target=testbox1
+        plc_box   ='speedball.inria.fr'
+        node_box1 = 'testbox64_1.onelab.eu'
+        node_box2 = 'testbox64_2.onelab.eu'
     elif options.arch == "x86_64":
-        testbox1 = 'testbox64_1.onelab.eu'
-        testbox2 = 'testbox64_2.onelab.eu'
-        target=testbox1
+        plc_box =   'speedball.inria.fr'
+        node_box1 = 'testbox64_1.onelab.eu'
+        node_box2 = 'testbox64_2.onelab.eu'
     else:
         print 'Unsupported arch %s'%options.arch
         sys.exit(1)
 
-    mapper = {'plc': [ ('*' , {'hostname':target,
-                               'PLC_DB_HOST':target,
-                               'PLC_API_HOST':target,
-                               'PLC_BOOT_HOST':target,
-                               'PLC_WWW_HOST':target,
+    mapper = {'plc': [ ('*' , {'hostname':plc_box,
+                               'PLC_DB_HOST':plc_box,
+                               'PLC_API_HOST':plc_box,
+                               'PLC_BOOT_HOST':plc_box,
+                               'PLC_WWW_HOST':plc_box,
                                'name':'%s-'+options.arch } ) 
                        ],
-              'node': [ ('deferred01' , {'host_box': testbox1 } ),
-                        ('deferred02' , {'host_box': testbox2 } ),
+              'node': [ ('deferred01' , {'host_box': node_box1 } ),
+                        ('deferred02' , {'host_box': node_box2 } ),
                         ],
               }
     
