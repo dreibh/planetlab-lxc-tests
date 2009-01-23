45d693e103f6ee9dbd89342182502777623dfc9c
Sapan Bhatia <sapanb@cs.princeton.edu>
2009-01-23 15:42:39 +0000

    Fixed planetflow test for Thierry's test environment.

--- qaapi/qa/tests/node/pf2test.pl
+++ qaapi/qa/tests/node/pf2test.pl
@@ -1,13 +1,16 @@
 #!/usr/bin/perl
 
+$netflow_slice="ple_netflow";
+
 print "Warning! This test runs only once. I.e., it will usually not run twice in succession till syslog sees some new activitt\n";
 print "Preparing...";
+
 
 system("cp /var/log/messages /tmp/pf2_test");
 
 # ping magic IP address
 print "Sending out packet...\n";
-system("su -c \"ping -c 1 64.34.177.39\" pl_netflow -"); # hm. Sapan doesn't own this IP anymore. We should change it at some point.
+system("su -c \"ping -I eth0 -c 1 10.0.0.8\" $netflow_slice -"); 
 
 print "Waiting for flow to appear...\n";
 sleep(10);
@@ -17,12 +20,19 @@ open PIP,"|/usr/bin/diff -u /tmp/pf2_test /var/log/messages";
 while (<PIP>) {
 	if (/Received test flow to corewars.org from slice ([0-9]+)/) {
 		$xid = int($1);
-		print "Slice id: $xid";
+		print "Slice id: $xid\n";
+        $success=1;
 		($xid==0 || $xid==-1) && die("Slice id incorrect");
 	}
 }
 
-close PIP;
+if ($success) {
+        print "[SUCCESS]";
+}
+else {
+        print "[FAILED]";
+}
 
-print "Test successful.";
+
+close PIP;
 
