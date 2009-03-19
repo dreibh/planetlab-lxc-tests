d1a0fab9041f4188e8272a11adbba60ab1f1d6e5
Sapan Bhatia <sapanb@cs.princeton.edu>
2009-03-19 16:51:33 +0000

    Cleaned up nmapitest.py

--- qaapi/qa/tests/slice/nmapitest.py
+++ qaapi/qa/tests/slice/nmapitest.py
@@ -4,7 +4,15 @@
 # Connect to the node manager
 # Author: acb@cs.princeton.edu
 
+import sys
+
 from xmlrpclib import ServerProxy
 
-nodemanager = ServerProxy('http://127.0.0.1:812/')
-nodemanager.SetLoans('pl_sirius', [])
+if (len(sys.argv)!=2):
+    print "[FAILED] Please pass the name of the slice this script runs in as the first argument.\n";
+else:
+    nodemanager = ServerProxy('http://127.0.0.1:812/')
+    try:
+        nodemanager.SetLoans(sys.argv[1], [])
+    except:
+        print "[FAILED] SetLoans didn't work, probably because the packets of this connection are not getting tagged.\n";
