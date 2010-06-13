d7b9830a34214a954b7eb7baf7af4e395e671543
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2010-06-13 15:29:13 +0200

    oops again

--- system/TestMain.py
+++ system/TestMain.py
@@ -187,7 +187,7 @@ steps refer to a method in TestPlc or to a step_* module
                 utils.header('* Using %s = %s'%(recname,getattr(self.options,recname)))
 
         # hack : if sfa is not among the published rpms, skip these tests
-        TestPlc.check_whether_build_has_sfa(options.arch_rpms_url)
+        TestPlc.check_whether_build_has_sfa(self.options.arch_rpms_url)
 
         # steps
         if not self.options.steps:
