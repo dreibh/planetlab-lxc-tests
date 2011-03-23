aaccdbe7a9305de045896a37a1f5e7e6bfdb75b3
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-03-23 15:21:40 +0100

    comments

--- system/config_triangle.py
+++ system/config_triangle.py
@@ -1,10 +1,10 @@
 # Thierry Parmentelat <thierry.parmentelat@inria.fr>
 # Copyright (C) 2010 INRIA 
 #
-# just overwrite options.size to be 2 and re-use the default config
+# re-use the default config with 3 myplc's and 
+# 3 SFA's for checking antiloop on some decent scale
 import config_default
 
-# for checking antiloop on some decent scale
 def config (plc_specs, options):
     options.size=3
     return config_default.config(plc_specs,options)
