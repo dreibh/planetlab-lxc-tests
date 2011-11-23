7e091116edb3263daceb18ef3ae3467999184722
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-11-23 18:56:43 +0100

    cleanup, should not need to set password nor sfa database details anymore

--- system/config_default.py
+++ system/config_default.py
@@ -274,7 +274,7 @@ def plc (options,index) :
         'PLC_MAIL_ENABLED':'false',
         'PLC_MAIL_SUPPORT_ADDRESS' : 'thierry.parmentelat@inria.fr',
         'PLC_DB_HOST' : 'deferred-myplc-hostname',
-        'PLC_DB_PASSWORD' : 'mnbvcxzlkjhgfdsapoiuytrewq',
+#        'PLC_DB_PASSWORD' : 'mnbvcxzlkjhgfdsapoiuytrewq',
         'PLC_API_HOST' : 'deferred-myplc-hostname',
         'PLC_WWW_HOST' : 'deferred-myplc-hostname',
         'PLC_BOOT_HOST' : 'deferred-myplc-hostname',
@@ -309,10 +309,10 @@ def sfa (options,index) :
         'SFA_PLC_PASSWORD' : 'test++',
         'SFA_DB_HOST':'deferred-myplc-hostname',
         'SFA_DB_USER' : 'pgsqluser',
-        'SFA_DB_PASSWORD' : 'mnbvcxzlkjhgfdsapoiuytrewq',
-        # the default settings come with 'sfa' as the dbname
-        # temporary until we get the initscript to create that one
-        'SFA_DB_NAME' : 'planetlab5',
+#        'SFA_DB_PASSWORD' : 'mnbvcxzlkjhgfdsapoiuytrewq',
+# the default settings come with 'sfa' as the dbname
+# temporary until we get the initscript to create that one
+#        'SFA_DB_NAME' : 'sfa',
         'SFA_API_DEBUG': True,
         # details of the slices to create
         'sfa_slice_specs' : [ sfa_slice_spec(options,index,rspec_style) 
