1b86bfd9f449f03c58410ac00a31ef3e14d31b2e
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2012-05-22 16:38:46 +0200

    -c sfavoid allows to test with flavour=void

---
+++ system/config_sfavoid.py
@@ -0,0 +1,12 @@
+# Thierry Parmentelat <thierry.parmentelat@inria.fr>
+# Copyright (C) 2012 INRIA 
+#
+# this config is defined to run on top of another one 
+# and just sets the generic flavour to 'void'
+
+def config (plc_specs, options):
+    def tweak_loglevel (plc_spec):
+        plc_spec['sfa']['SFA_GENERIC_FLAVOUR'] = 'void'
+        plc_spec['sfa']['SFA_AGGREGATE_ENABLED'] = 'false'
+        return plc_spec
+    return [ tweak_loglevel (plc_spec) for plc_spec in plc_specs ]
