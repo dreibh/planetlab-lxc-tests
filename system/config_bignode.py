2ce3c411bd50cf2b303d3fc440e8f918d311f735
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2012-11-28 09:24:20 +0100

    oops, bignode was broken

--- system/config_bignode.py
+++ system/config_bignode.py
@@ -5,7 +5,7 @@
 
 def config (plc_specs, options):
     def tweak_timers (plc_spec):
-        plc_spec['ssh_node_boot_timers'] (60, 58)
+        plc_spec['ssh_node_boot_timers']=(60, 58)
         return plc_spec
     return [tweak_timers (plc_spec) for plc_spec in plc_specs ]
 
