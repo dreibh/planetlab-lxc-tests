49ed1e07b0dbf73a2a57543f914825d202af2b68
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-02-01 22:06:29 +0100

    disabling deathvegas for now

--- system/LocalSubstrate.inria.py
+++ system/LocalSubstrate.inria.py
@@ -30,7 +30,7 @@ class OnelabSubstrate (Substrate):
    # the lxc-capable box for PLCs
    def plc_lxc_boxes_spec (self):
       return [ ('gotan', 35),  # how many plcs max in this box 
-               ('deathvegas', 35),  
+#               ('deathvegas', 35),  
                ]  
 
    # vplc01 to 40
