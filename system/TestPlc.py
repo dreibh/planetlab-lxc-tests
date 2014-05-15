59a844c8415526a633e9c082b208c6b4d0fe7043
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-05-15 09:47:01 +0200

    ignore result of slice_fs_deleted

--- system/TestPlc.py
+++ system/TestPlc.py
@@ -162,7 +162,7 @@ class TestPlc:
         'ssh_slice_sfa@1', 'sfa_delete_slice@1', 'sfa_delete_user@1', SEPSFA,
         'cross_check_tcp@1', 'check_system_slice', SEP,
         # check slices are turned off properly
-        'empty_slices', 'ssh_slice_off', 'slice_fs_deleted', SEP,
+        'empty_slices', 'ssh_slice_off', 'slice_fs_deleted_ignore', SEP,
         # check they are properly re-created with the same name
         'fill_slices', 'ssh_slice_again_ignore', SEP,
         'gather_logs_force', SEP,
