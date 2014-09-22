d877bf4d95cecaa52b404cf02f6b2103b2334c05
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-09-22 16:30:03 +0200

    estran : max vnodes 3->2, too small disk

--- system/LocalSubstrate.inria.py
+++ system/LocalSubstrate.inria.py
@@ -51,7 +51,7 @@ class OnelabSubstrate (Substrate):
 # nodes spawned in this box won't get network connectivity
 #         ('enfoui', 4), # 4 cores, 8Gb
 # estran (4) - big mem but small disk
-         ('estran', 3), # 4 cores, 8Gb
+         ('estran', 2), # 4 cores, 8Gb
 # lodos (5) - rather old/small
          ('lodos', 1), # 2 cores, 4Gb
 # cyblok (6)        
