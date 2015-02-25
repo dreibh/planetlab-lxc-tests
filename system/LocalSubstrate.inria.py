0cdcaadc2261884905d38308ce847217127297dd
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2015-02-25 16:34:50 +0100

    move to another box

--- system/LocalSubstrate.inria.py
+++ system/LocalSubstrate.inria.py
@@ -40,7 +40,7 @@ class OnelabSubstrate (Substrate):
 
    def qemu_boxes_spec (self):
       return [ # (hostname, how many qemus max in this box)
-         ('buzzcocks', 12),
+         ('boxtops', 12),
          ]
 
 # these boxes are going on a well deserved retirement
